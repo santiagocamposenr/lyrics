991e91db607ac63845861c925fae794992fde4b4
santiaocamposenr <santiago.campos.enriquez@gmail.com>
2020-07-16 13:31:02 -0500

    Updates

---
+++ precache-manifest.9aef8fe1dbe1a5f39cb885420af12f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93648cc2dd0405b54dc13bbfdd52133b",
+    "url": "/lyrics/index.html"
+  },
+  {
+    "revision": "4f5624184e9357a78dda",
+    "url": "/lyrics/static/css/main.f54c6362.chunk.css"
+  },
+  {
+    "revision": "7b79d8c82a5b129311be",
+    "url": "/lyrics/static/js/2.96603fb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lyrics/static/js/2.96603fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5624184e9357a78dda",
+    "url": "/lyrics/static/js/main.a15a9238.chunk.js"
+  },
+  {
+    "revision": "ee790b2117f39b151f64",
+    "url": "/lyrics/static/js/runtime-main.a6e5084f.js"
+  }
+]);
